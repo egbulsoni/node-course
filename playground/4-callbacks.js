b9e1d57bf4f26cfd22b05969f7794554b5131cd1
Eduardo <egbulsoni@gmail.com>
2019-10-25 17:23:45 -0300

    reading files from mongo

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,34 +1,13 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
-
-const names = ['Andrew', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
-
-const geocode = (address, callback) => {
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        const data = {
-            latitude: 0,
-            longitude: 0
-        }
-    
-        callback(data)
+        //callback('This is my error', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-geocode('Philadelphia', (data) => {
-    console.log(data)
-})
-
-
-const add = (num1, num2, callback) => {
-    setTimeout(() => {
-        callback(num1 + num2)
-    }, 2000)
-}
-
-add (1,4, (sum) => {
-    console.log(sum)
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+    console.log(result)
 })
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -20,62 +20,33 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Vikram',
-        age: 26
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
+    // db.collection('users').findOne({ _id: new ObjectID("5db34d9743787c388374823c")}, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
 
-        console.log(result.ops)
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ age: 22 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    // db.collection('users').find({ age: 22 }).count((error, count) => {
+    //     console.log(count)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5daceb4da928bd41ef35722a")}, (error, task) => {
+        if (error) {
+            return console.log('Unable to fetch data')
+        }
+        console.log(task)
     })
 
-    db.collection('users').insertOne({
-        name: 'Eduardo',
-        age: 22
-    }, (error, result) => {
+    db.collection('tasks').find({ completed: false}).toArray((error, tasks) => {
         if (error) {
-            return console.log('Unable to insert user')
+            return console.log('Unable to fetch data')
         }
-
-        console.log(result.ops)
+        console.log(tasks)
     })
-    db.collection('users').insertMany([
-        {
-            name: 'Jen',
-            age: 28
-        }, {
-            name: 'Gunther',
-            age: 27
-        }
-    ]), (error, result) => {
-        if (error) {
-            return console.log('Unable to insert documents!')
-        }
-
-        console.log(result.ops)
-    }
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'feed the dog',
-            completed: true
-        },
-        {
-            description: 'learn the guitar',
-            completed: false
-        },
-        {
-            description: 'learn nodejs',
-            completed: true
-        }
-    ]), (error, result) => {
-        if (error) {
-            return console.log('unable to insert documents!')
-        }
-
-        console.log(result.ops)
-    }
 })
